657bfe80e4e635e9afb8e92bebc34eb91a9bef80
Axely7 <axely7@hotmail.com>
2024-07-15 15:24:23 -0600

    http request

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,18 +1,46 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
   private _tagsHistory: string[] = [];
+  private apiKey: string = '3LmJBVfXhhvzIseVMiqWpD6azDQgewLm';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagsHistory(){
     return [...this._tagsHistory];
   }
 
-  searchTag(tag: string): void{
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if(this._tagsHistory.includes(tag)){
+      this._tagsHistory = this._tagsHistory.filter(old => old !== tag)
+    }
+
     this._tagsHistory.unshift(tag);
-    console.log(this.tagsHistory);
+
+    this._tagsHistory = this._tagsHistory.slice(0, 10)
+
+  }
+
+  searchTag(tag: string): void{
+    if(tag.length === 0) return;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', tag)
+
+
+    this.http.get(`${this.serviceUrl}/search`, {params})
+    .subscribe(resp => {
+      console.log(resp)
+    })
 
   }
 
